4daff149504fcb69936a21679786f41b6b1e1532
divine-mercyx <divineobinali9@gmail.com>
2026-07-18 19:19:30 +0100

    add fathers_past_age_calculator.py file

---
+++ orion afterparty/fathers_past_age_calculator.py
@@ -0,0 +1,4 @@
+father_age = int(input("enter fathers  age: "))
+son_age = int(input("enter sons age: "))
+years_father_was_twice_as_old = father_age-(son_age * 2)
+print("the father was twice as old as his son : ",years_father_was_twice_as_old,"years ago")
